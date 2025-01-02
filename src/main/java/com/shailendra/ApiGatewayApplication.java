8f93ee25091bbb97b6e61e660a031c9d38ad878a
styagi14 <67920863+styagi14@users.noreply.github.com>
2025-01-02 16:14:18 +0400

    Update ApiGatewayApplication.java

--- src/main/java/com/shailendra/ApiGatewayApplication.java
+++ src/main/java/com/shailendra/ApiGatewayApplication.java
@@ -9,6 +9,7 @@ public class ApiGatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
         System.out.println("Hello World !!");
+        System.out.println("######");
     }
 
 }
